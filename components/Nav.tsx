873ea1714eb96de711398319441101e155ff3487
mariuszaepbiznes-ops <mariuszaep.biznes@gmail.com>
2026-09-21 15:30:01 +0200

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -10,7 +10,7 @@ export default function Nav() {
     {
       id: 'hero',
       label: 'Wstęp',
-      href: '#hero',
+      href: '#wstep',
       icon: (
         <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
           <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
