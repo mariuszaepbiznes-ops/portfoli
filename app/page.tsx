3a9bca928f19c3322254446837b777120c9ad264
mariuszaepbiznes-ops <mariuszaep.biznes@gmail.com>
2026-09-21 15:27:35 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Nav from '@/components/Nav'
-import Hero from '@/components/Hero'
+import Hero from '@/components/wstep'
 import About from '@/components/About'
 import Projects from '@/components/Projects'
 import Pricing from '@/components/Pricing'
